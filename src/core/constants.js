2dc568e4818c9073d28a3a5fe2e6b4b79281a358
Oliver <lyahcer@hotmail.com>
2026-05-07 00:22:10 +0800

    step 9: split constants into core module

---
+++ src/core/constants.js
@@ -0,0 +1,63 @@
+window.XM = window.XM || {};
+
+window.XM.Constants = window.XM.Constants || {
+  APP_STATE: Object.freeze({
+    LOBBY: "LOBBY",
+    LOADOUT: "LOADOUT",
+    DEPLOY: "DEPLOY",
+    BATTLE: "BATTLE",
+    LEVEL_UP_REWARD: "LEVEL_UP_REWARD",
+    SETTLEMENT: "SETTLEMENT",
+  }),
+
+  ENEMY_STATE: Object.freeze({
+    MOVING: "MOVING",
+    ATTACKING: "ATTACKING",
+    DEAD: "DEAD",
+  }),
+
+  colors: Object.freeze({
+    "剑": "#d7e1ec",
+    "火": "#ef7b45",
+    "体": "#d6b36a",
+    "冰": "#72c8ee",
+    "毒": "#a97ad8",
+    "雷": "#c4b5fd",
+    enemy_little_yao: "#85a96e",
+    enemy_swift_wolf: "#98d6cb",
+    enemy_armor_beast: "#7b7d8d",
+    enemy_blood_cultivator: "#c45252",
+    boss_blackwind: "#555f6f",
+    boss_bloodlotus: "#bb3d84",
+    boss_outer_demon: "#6d28d9",
+    剑修: "#d7e1ec",
+    火修: "#ef7b45",
+    冰修: "#72c8ee",
+    毒修: "#a97ad8",
+    雷修: "#c4b5fd",
+    枪修: "#d6b36a",
+    音修: "#f0abfc",
+    暗修: "#a78bfa",
+    万法: "#facc15",
+  }),
+
+  rarityWeight: Object.freeze({
+    "普通": 70,
+    "稀有": 24,
+    "史诗": 8,
+    "传说": 2,
+  }),
+
+  CHARACTER_RARITY: Object.freeze({
+    SR: "SR",
+    SSR: "SSR",
+    UR: "UR",
+    SP: "SP",
+  }),
+
+  GACHA_COST: 200,
+  DUPLICATE_GACHA_REFUND: 50,
+  DEBUG_STORAGE_KEY: "xuanmen_debug_runtime",
+  DEBUG_OVERRIDES_KEY: "xuanmen_debug_overrides",
+  DEBUG_TABS: Object.freeze(["状态", "角色", "先天武学", "机缘 / 升级候选", "怪物", "波次", "法宝", "护山大阵"]),
+};
--- src/game.js
+++ src/game.js
@@ -50,67 +50,21 @@ const debugTabs = document.querySelector("#debugTabs");
 const debugActionsPanel = document.querySelector("#debugActions");
 const debugContent = document.querySelector("#debugContent");
 
-const APP_STATE = {
-  LOBBY: "LOBBY",
-  LOADOUT: "LOADOUT",
-  DEPLOY: "DEPLOY",
-  BATTLE: "BATTLE",
-  LEVEL_UP_REWARD: "LEVEL_UP_REWARD",
-  SETTLEMENT: "SETTLEMENT",
-};
-
-const ENEMY_STATE = {
-  MOVING: "MOVING",
-  ATTACKING: "ATTACKING",
-  DEAD: "DEAD",
-};
-
-const colors = {
-  "剑": "#d7e1ec",
-  "火": "#ef7b45",
-  "体": "#d6b36a",
-  "冰": "#72c8ee",
-  "毒": "#a97ad8",
-  "雷": "#c4b5fd",
-  enemy_little_yao: "#85a96e",
-  enemy_swift_wolf: "#98d6cb",
-  enemy_armor_beast: "#7b7d8d",
-  enemy_blood_cultivator: "#c45252",
-  boss_blackwind: "#555f6f",
-  boss_bloodlotus: "#bb3d84",
-  boss_outer_demon: "#6d28d9",
-  剑修: "#d7e1ec",
-  火修: "#ef7b45",
-  冰修: "#72c8ee",
-  毒修: "#a97ad8",
-  雷修: "#c4b5fd",
-  枪修: "#d6b36a",
-  音修: "#f0abfc",
-  暗修: "#a78bfa",
-  万法: "#facc15",
-};
-
-const rarityWeight = {
-  "普通": 70,
-  "稀有": 24,
-  "史诗": 8,
-  "传说": 2,
-};
-
-const CHARACTER_RARITY = {
-  SR: "SR",
-  SSR: "SSR",
-  UR: "UR",
-  SP: "SP",
-};
+const {
+  APP_STATE,
+  ENEMY_STATE,
+  colors,
+  rarityWeight,
+  CHARACTER_RARITY,
+  GACHA_COST,
+  DUPLICATE_GACHA_REFUND,
+  DEBUG_STORAGE_KEY,
+  DEBUG_OVERRIDES_KEY,
+  DEBUG_TABS,
+} = window.XM.Constants;
 
 const PLAYER_LEVEL_UNLOCKS = DATA.playerLevelUnlocks || [];
 const DEPLOY_SLOT_UNLOCKS = DATA.deploySlotUnlocks || [];
-const GACHA_COST = 200;
-const DUPLICATE_GACHA_REFUND = 50;
-const DEBUG_STORAGE_KEY = "xuanmen_debug_runtime";
-const DEBUG_OVERRIDES_KEY = "xuanmen_debug_overrides";
-const DEBUG_TABS = ["状态", "角色", "先天武学", "机缘 / 升级候选", "怪物", "波次", "法宝", "护山大阵"];
 let activeDebugTab = "状态";
 let debugEditMode = false;
 let debugExportOpen = false;
